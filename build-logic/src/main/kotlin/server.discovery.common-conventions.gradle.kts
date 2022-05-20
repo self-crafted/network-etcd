052b10918cd51dc36e5749c8ec81443cebf3469a
offbyone <offbyone@disroot.org>
2022-05-20 17:09:21 +0200

    Use JitPack for publication

--- build-logic/src/main/kotlin/server.discovery.common-conventions.gradle.kts
+++ build-logic/src/main/kotlin/server.discovery.common-conventions.gradle.kts
@@ -18,16 +18,6 @@ tasks {
         }
     }
     publishing {
-        repositories {
-            maven {
-                name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/self-crafted/network-etcd")
-                credentials {
-                    username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                    password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
-                }
-            }
-        }
         publications {
             register<MavenPublication>("gpr") {
                 from(components["java"])
